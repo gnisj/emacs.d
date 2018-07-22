79f3b3a84cdc1b79f58c87a322a64864ee814798
gnisj <gnisj@protonmail.com>
2018-07-22 16:35:17 +0200

    Added show col number

--- init.el
+++ init.el
@@ -13,8 +13,17 @@
  ;;'(default ((t (:inherit nil :stipple nil :background "gray17" :foreground "lime green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "Ubuntu Mono")))))
 
 (global-linum-mode t)
+
+;; Display current column number
+(setq column-number-mode t)
+
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
+
+;; Added to get hideshow feature to work 
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+(global-set-key (kbd "C-c <right>") 'hs-show-block)
+(global-set-key (kbd "C-c <left>") 'hs-hide-block)
